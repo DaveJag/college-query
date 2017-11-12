9b2be46a3a399c81925df0ee081715cdcca87182
DaveJag <davejag@comcast.net>
2017-11-11 19:02:09 -0500

    Have webpack working now to create new bundle.js files as we develop.

--- app/components/children/Saved.jsx
+++ app/components/children/Saved.jsx
@@ -4,10 +4,10 @@
 
 
 // Include React
-var React = require(“react”);
+var React = require("react");
 
 // Require helper for making API calls
-var helpers = require(“../utils/helpers.js”);
+var helpers = require("../utils/helpers.js");
 
 // Create search component
 var Saved = React.createClass({
@@ -24,7 +24,7 @@ var Saved = React.createClass({
    // Collect clicked article’s id
    var schoolMongoId = event.target.value;
 
-   // Copy “this” into “that” so component is accessible inside functions.
+   // Copy "this" into "that" so component is accessible inside functions.
    var that = this;
 
    // Send this data to API endpoint to save it to Mongo
@@ -47,28 +47,28 @@ var Saved = React.createClass({
 
    return (
 
-     <div className=“panel panel-default”>
+     <div className="panel panel-default">
 
-       <div className=“panel-heading”>
-         <h3 className=“panel-title text-center” style={ {fontSize: “20px”} }><i><b>Saved</b></i></h3>
+       <div className="panel-heading">
+         <h3 className="panel-title text-center" style={ {fontSize: "20px"} }><i><b>Saved</b></i></h3>
        </div>
 
-       <div className=“panel-body”>
-         <ul className=“list-group col-md-8 col-md-offset-2">
+       <div className="panel-body">
+         <ul className="list-group col-md-8 col-md-offset-2">
 
            {/* ++++++++++++++++++++++++++++++++ ITERATE HERE ++++++++++++++++++++++++++++++++ */}
            {/* Use map function to loop through array in JSX */}
            {this.props.mongoResults.map(function(search, i) {
 
              return (
-               <li key={search._id} className=“list-group-item” style={ {borderWidth: “0px”} }>
-                 <div className=“input-group”>
-                   <div type=“text” className=“form-control”>
-                     <b><a href={search.url} target=“_new” style={ {color: “black”} }>{search.programs}</a></b>
+               <li key={search._id} className="list-group-item" style={ {borderWidth: "0px"} }>
+                 <div className="input-group">
+                   <div type="text" className="form-control">
+                     <b><a href={search.url} target="_new" style={ {color: "black"} }>{search.programs}</a></b>
                      <i> {search.date.substring(0, 10)}</i>
                    </div>
-                   <span className=“input-group-btn”>
-                     <button className=“btn btn-danger” type=“button” onClick={that._handleDelete} value={search._id}>Remove</button>
+                   <span className="input-group-btn">
+                     <button className="btn btn-danger" type="button" onClick={that._handleDelete} value={search._id}>Remove</button>
                    </span>
                  </div>
                </li>
